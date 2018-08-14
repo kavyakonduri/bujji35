cbc4b1bc481e43979819698891e354d16bf0edf4
kavyakonduri <41731461+kavyakonduri@users.noreply.github.com>
2018-08-14 09:55:51 +0530

    Create kavya25.java

---
+++ kavya25.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        String string=sc.next();
+        boolean numeric=true;
+        try
+        {
+           double num=Double.parseDouble(string);
+        }catch(NumberFormatException e) {
+          numeric=false;
+       }
+      if(numeric)
+         System.out.println(string +"is a number");
+      else
+         System.out.println(string +"is not a number");
+   }
+}
